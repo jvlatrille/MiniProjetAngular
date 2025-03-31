bb65e0de7a2e4d64b247d41166460e4619beeaa4
jvlatrille <julesvinet64@gmail.com>
2025-03-31 10:44:19 +0200

    L'API LARAVEL FONCTIONNE !!!!!!

--- API_Laravel/app/Providers/AppServiceProvider.php
+++ API_Laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +14,16 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
     }
 }
